483d89010327720d78ff8ec9956ea33800353df9
Akshay Kolli <proflepton@gmail.com>
2020-09-08 13:24:08 +0530

    firstPush
    Added Idle animation, basic movement, Improved jump, placeholder tile assets.
    No ropeswing mechanic yet.

---
+++ project-58/Assets/Scripts/MonkeyMain.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyMain : MonoBehaviour
+{
+    //Getting componenets
+    private Rigidbody2D playerRB;
+
+
+    //Preference Variables
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private float fallJumpMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    [SerializeField] private LayerMask platform;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        if(horizontal !=0)
+        {
+            playerRB.velocity = new Vector2(horizontal * runSpeed, playerRB.velocity.y);
+        }
+
+        if(Input.GetButton("Jump"))
+        {
+            if(playerRB.IsTouchingLayers(platform))
+            {
+                playerRB.velocity = Vector2.up * jumpSpeed;
+            }
+            
+        }
+
+        if (playerRB.velocity.y < 0)
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (fallJumpMultiplier - 1) * Time.deltaTime;
+        }
+        else if (playerRB.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    
+    }
+
+}
